b040d1a440590317c742102aa44e3b6fb216dea2
Mukhammadnur <gsjxne@gmail.com>
2024-04-11 18:36:53 +0300

    init

---
+++ src/main/java/com/greenatom/noticeboards/noticeboards/NoticeBoardsApplication.java
@@ -0,0 +1,13 @@
+package com.greenatom.noticeboards.noticeboards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeBoardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticeBoardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenatom/noticeboards/noticeboards/NoticeBoardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenatom.noticeboards.noticeboards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeBoardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
